901c412cb6a6d8d6d81991c0dda09adeeb93e7f2
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-08-13 18:04:41 -0700

    Completed mini weather reporting application

--- src/components/googlemaps.js
+++ src/components/googlemaps.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
 class GoogleMap extends Component {
-
   componentDidMount() {
     new google.maps.Map(this.refs.map, {
       zoom: 12,
-      center: new google.maps.LatLng(this.props.lat,this.props.lon)     
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
     });
   }
 
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -15,7 +15,7 @@ class WeatherList extends Component {
 
     return (
       <tr key={index}>
-        <td>GoogleMap lon={lon} lat={lat}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange"  units="K" /></td>
         <td><Chart data={pressures} color="green"  units="hPa" /></td>
         <td><Chart data={humdities} color="black" units="%" /></td>
